ab14d1444430efdcef167fcace21ec0a238fb398
John Mettraux <jmettraux@gmail.com>
2014-07-18 07:08:08 +0900

    scaffold the request parser

--- src/shervin_req.c
+++ src/shervin_req.c
@@ -25,9 +25,62 @@
 
 #define _POSIX_C_SOURCE 200809L
 
+#include "aabro.h"
+
+
+abr_parser *request_parser = NULL;
+
+void shv_init_parser()
+{
+  if (request_parser != NULL) return;
+
+  abr_parser *sp = abr_string(" ");
+  abr_parser *crlf = abr_string("\r\n");
+  abr_parser *lws = abr_regex("\r\n[ \t]*");
+
+  abr_parser *token =
+    abr_regex("[^\(\)<>@,;:\\\"\/\[\]\?=\{\} \t]+");
+
+  abr_parser *method =
+    abr_n_alt(
+      "method",
+      abr_string("OPTIONS"), abr_string("GET"), abr_string("HEAD"),
+      abr_string("POST"), abr_string("PUT"), abr_string("DELETE"),
+      abr_string("TRACE"), abr_string("CONNECT"),
+      abr_name("extension_method", token),
+      NULL);
+  abr_parser *request_uri =
+    NULL;
+  abr_parser *http_version =
+    abr_n_regex("http_version", "^HTTP\/\d+\.\d+");
+
+  abr_parser *request_line =
+    abr_seq(method, sp, request_uri, sp, http_version, crlf, NULL);
+
+  abr_parser *field_name = abr_name("field_name", token);
+  abr_parser *field_value = NULL;
+
+  abr_parser *message_header =
+    abr_seq(field_name, abr_string(":"), field_value, NULL);
+
+  abr_parser *message_body =
+    NULL;
+
+  request_parser =
+    abr_seq(
+      request_line,
+      abr_rep(
+        abr_seq(message_header, crlf, NULL),
+        0, -1),
+      crlf,
+      abr_rep(message_body, 0, 1),
+      NULL);
+}
 
 shv_request *shv_parse_request(char *s)
 {
+  shv_init_parser();
+
   shv_request *req = calloc(1, sizeof(shv_request));
 
   return req;
