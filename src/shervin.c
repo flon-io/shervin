326e252e27dfd7f6fac8e9585ecf064d692ee163
John Mettraux <jmettraux@gmail.com>
2014-08-31 14:28:15 +0900

    add headers necessary for inet_ntoa()

--- src/shervin.c
+++ src/shervin.c
@@ -29,7 +29,9 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <ev.h>
 
 #include "flutil.h"
--- src/shv_response.c
+++ src/shv_response.c
@@ -28,7 +28,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <ev.h>
 
 #include "gajeta.h"
