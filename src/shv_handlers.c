86485af7844ef48f1831e756228535689e55db6c
John Mettraux <jmettraux@gmail.com>
2014-11-10 09:20:42 +0900

    spin shv_guards out of shv_handlers

--- src/shv_handlers.c
+++ src/shv_handlers.c
@@ -25,101 +25,17 @@
 
 // https://github.com/flon-io/shervin
 
-// handlers, and guards
+// handlers
 
 #define _POSIX_C_SOURCE 200809L
 
-#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
 
 #include "gajeta.h"
 #include "shervin.h"
 #include "shv_protected.h"
 
-
-//
-// guards
-
-int shv_filter_guard(shv_request *req, shv_response *res, flu_dict *params)
-{
-  return 1;
-}
-
-int shv_any_guard(shv_request *req, shv_response *res, flu_dict *params)
-{
-  return 1;
-}
-
-int shv_path_guard(shv_request *req, shv_response *res, flu_dict *params)
-{
-  char *path = (char *)flu_list_get(params, "path");
-  char *rpath = (char *)flu_list_get(req->uri_d, "_path");
-
-  //printf("path >%s<\nrpath >%s<\n", path, rpath);
-
-  if (*path != '/')
-  {
-    char m = tolower(path[0]);
-    if (tolower(path[1]) == 'u') m = 'u';
-    char rm = req->method;
-    if (rm == 'h') rm = 'g';
-    if (m != rm) return 0;
-    path = strchr(path, ' ') + 1;
-  }
-
-  int success = 1;
-
-  while (1)
-  {
-    char *slash = strchr(path, '/');
-    char *rslash = strchr(rpath, '/');
-
-    if (slash == NULL) slash = strchr(path, '\0');
-    if (rslash == NULL) rslash = strchr(rpath, '\0');
-
-    if (*path == ':')
-    {
-      char *k = strndup(path + 1, slash - path - 1);
-      flu_list_set(req->routing_d, k, strndup(rpath, rslash - rpath));
-      free(k);
-    }
-    else if (strcmp(path, "**") == 0)
-    {
-      flu_list_set(req->routing_d, "**", strdup(rpath)); break;
-    }
-    else
-    {
-      //printf("s - p : %zu / rs - rp : %zu\n", slash - path, rslash - rpath);
-      //printf("p >%s<  rp >%s<\n", path, rpath);
-      if (slash - path != rslash - rpath) { success = 0; break; }
-      if (strncmp(path, rpath, slash - path) != 0) { success = 0; break; }
-    }
-
-    if (*slash == 0 && *rslash == 0) break;
-
-    if (*rslash == 0 && strcmp(slash, "/**") == 0)
-    {
-      flu_list_set(req->routing_d, "**", strdup("")); break;
-    }
-    if (*slash == 0 || *rslash == 0)
-    {
-      success = 0; break;
-    }
-
-    path = slash + 1;
-    rpath = rslash + 1;
-  }
-
-  return success;
-}
-
-
-//
-// handlers
 
 static char *shv_determine_content_type(char *path)
 {
