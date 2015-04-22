67d013f693981948a5305e1539ab862951d80309
John Mettraux <jmettraux@gmail.com>
2015-04-23 06:40:11 +0900

    restart

--- src/shervin.h
+++ src/shervin.h
@@ -33,7 +33,7 @@
 #include "flutil.h"
 
 
-#define FSHV_VERSION "1.0.0"
+#define FSHV_VERSION "1.1.0"
 
 #define FSHV_BUFFER_SIZE 4096
 
@@ -47,12 +47,7 @@ typedef struct {
   flu_dict *uri_d;
   flu_dict *headers;
   char *body;
-  short status_code; // 4xx code set by shervin, 200 else
-  flu_dict *routing_d; // used by guards to pass info to handlers
 } fshv_request;
-
-char fshv_method_to_char(char *s);
-char *fshv_char_to_method(char c);
 
 // response
 
@@ -62,101 +57,27 @@ typedef struct {
   flu_list *body;
 } fshv_response;
 
-// route
-
-typedef int fshv_handler(
-  fshv_request *req, fshv_response *res, int mode, flu_dict *params);
+// env
 
 typedef struct {
-  fshv_handler *guard;
-  fshv_handler *handler;
-  flu_dict *params;
-} fshv_route;
+  flu_dict *conf; // application level conf
+  fshv_request *req;
+  fshv_response *res;
+  flu_dict *bag; // req-res scoped databag
+} fshv_env;
 
-fshv_route *fshv_route_malloc(fshv_handler *guard, fshv_handler *handler, ...);
-#define fshv_r(...) fshv_route_malloc(__VA_ARGS__)
+// handler
 
-fshv_route *fshv_rp(char *path, fshv_handler *handler, ...);
-
-
-enum // flags guards and handlers "mode"
-{
-  FSHV_F_NULL_GUARD = 1 << 0, // only for handlers, set when guard was NULL
-  FSHV_F_HANDLED    = 1 << 1, // set when a previous "handled" the request
-};
+typedef int fshv_handler(fshv_env *env, flu_dict *params);
 
 // guards
 
-/* Merely a marker function, corresponding handlers are called as filters.
- */
-int fshv_filter_guard(
-  fshv_request *req, fshv_response *res, int mode, flu_dict *params);
+int fshv_match(fshv_env *env, char *route);
 
-int fshv_any_guard(
-  fshv_request *req, fshv_response *res, int mode, flu_dict *params);
-int fshv_path_guard(
-  fshv_request *req, fshv_response *res, int mode, flu_dict *params);
+// serve
 
-
-// handlers
-
-/* Used by shv_dir_handler(), public since it could get useful on its own.
- */
-ssize_t fshv_serve_file(
-  fshv_response *res, flu_dict *params, const char *path, ...);
-
-int fshv_dir_handler(
-  fshv_request *req, fshv_response *res, int mode, flu_dict *params);
-
-
-// filters
-
-typedef int fshv_authenticate(
-  const char *user, const char *pass, fshv_request *req, flu_dict *params);
-
-void fshv_set_user(fshv_request *req, const char *auth, const char *user);
-char *fshv_get_user(fshv_request *req, const char *auth);
-
-int fshv_basic_auth_filter(
-  fshv_request *req, fshv_response *res, int mode, flu_dict *params);
-
-int fshv_session_auth_filter(
-  fshv_request *req, fshv_response *res, int mode, flu_dict *params);
-
-/* Used by login endpoints to start a session.
- */
-void fshv_start_session(
-  fshv_request *req, fshv_response *res, flu_dict *params, const char *user);
-
-/* Used by logout endpoints to leave a session.
- */
-void fshv_stop_session(
-  fshv_request *req, fshv_response *res, flu_dict *params, const char *sid);
-
-
-// serving
-
-void fshv_serve(int port, fshv_route **routes);
-  // NULL terminated route array
-
-// TODO: fshv_stop_serve?
-
-
-// ... new order ...
-
-typedef struct {
-  fshv_request *req;
-  fshv_response *res;
-  flu_dict *dict;
-} fshv_env;
-
-//typedef void fshv_n_handler(fshv_env *env);
-
-//void fshv_serve(int port, fshv_n_handler *root_handler);
+void fshv_serve(int port, fshv_handler *root_handler, flu_dict *conf);
   // interfaces?
-
-int fshv_match_route(fshv_env *env, char *route);
-#define fshv_mr(env, route) fshv_match_route(env, route);
 
 #endif // FLON_SHERVIN_H
 
